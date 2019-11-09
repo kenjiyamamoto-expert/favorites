292a37f5568ad3f6eaa9a37ec66c99f776dfa655
kenji <totyu_go2y@yahoo.co.jp>
2019-11-09 16:10:15 +0900

    お気に入り登録画面の作成

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,8 @@
 class FavoritesController < ApplicationController
   def index
   end
+
+  def new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: "favorites#index"
+
+  get '/new'=> "favorites#new"
 end
