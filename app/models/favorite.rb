5397a335ca65d57dcda407aeb27a9afceb4a8762
kenji <totyu_go2y@yahoo.co.jp>
2019-11-16 15:18:16 +0900

    モデルの編集
    バリデーションの設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,3 +1,8 @@
 class Favorite < ApplicationRecord
   mount_uploader :image, ImageUploader
+
+  validates :name,presence: true,length:{maximum: 20}
+  validates :url,presence: true
+  validates :image,presence: true
+  validates :text,length:{maximum: 140}
 end
