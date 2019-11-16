5da29e6a09a9270b25168083e952be5ffcb73954
kenji <totyu_go2y@yahoo.co.jp>
2019-11-16 18:35:34 +0900

    モデルの編集

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,8 +1,8 @@
 class Favorite < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  validates :name,presence: true,length:{maximum: 20}
-  validates :url,presence: true
+  validates :name,presence: true,length:{maximum: 20},uniqueness: true
+  validates :url,presence: true,uniqueness: true
   validates :image,presence: true
   validates :text,length:{maximum: 140}
 end
