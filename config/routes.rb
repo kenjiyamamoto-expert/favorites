5af76c45224a1d7b2044118ff40b79dbc4d1cfd5
kenji <totyu_go2y@yahoo.co.jp>
2019-11-25 17:39:09 +0900

    setting unicorn

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "favorites#index"
 
-  get '/new'=> "favorites#new"
-
-  get '/edit'=> "favorites#edit"
-  get '/completion_registration'=> "favorites#completion_registration"
-  get '/completion_edit'=> "favorites#completion_edit"
-  get '/completion_delete'=> "favorites#completion_delete"
 
   resources :favorites do
     collection do
