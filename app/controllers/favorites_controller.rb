e66b7fdb8f07dc5378c3fc6361a7d1332cc8726d
kenji <totyu_go2y@yahoo.co.jp>
2019-12-01 18:55:15 +0900

    初期データを表示
    おすすめサイト（初期データ）をトップページに表示させる

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,7 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @recommended= Favorite.order("created_at DESC").page(params[:page]).per(20)
   end
 
   def new
